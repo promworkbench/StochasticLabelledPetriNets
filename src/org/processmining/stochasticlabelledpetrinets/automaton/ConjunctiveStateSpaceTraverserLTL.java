a7eb70497e78ffba4da387c013c33dfb889939ed
sleemans <s.leemans@bpm.rwth-aachen.de>
2022-03-01 21:59:00 +0000

    add LTL impl skeleton
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticLabelledPetriNets/Trunk@45938 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/stochasticlabelledpetrinets/automaton/ConjunctiveStateSpaceTraverserLTL.java
@@ -0,0 +1,22 @@
+package org.processmining.stochasticlabelledpetrinets.automaton;
+
+import org.processmining.ltl2automaton.plugins.automaton.State;
+
+public class ConjunctiveStateSpaceTraverserLTL extends ConjunctiveStateSpaceTraverser<State> {
+
+	public State getInitialState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public State takeStep(State state, String label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isFinalState(State state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
