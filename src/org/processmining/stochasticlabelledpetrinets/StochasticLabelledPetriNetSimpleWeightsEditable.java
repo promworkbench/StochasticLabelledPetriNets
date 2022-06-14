650fcd9a492768b9e99dcf47abac3a338f9ab29b
sleemans <s.leemans@bpm.rwth-aachen.de>
2022-06-14 20:01:44 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticLabelledPetriNets/Trunk@46384 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/stochasticlabelledpetrinets/StochasticLabelledPetriNetSimpleWeightsEditable.java
+++ src/org/processmining/stochasticlabelledpetrinets/StochasticLabelledPetriNetSimpleWeightsEditable.java
@@ -1,6 +1,7 @@
 package org.processmining.stochasticlabelledpetrinets;
 
-public interface StochasticLabelledPetriNetSimpleWeightsEditable extends StochasticLabelledPetriNetSimpleWeights {
+public interface StochasticLabelledPetriNetSimpleWeightsEditable
+		extends StochasticLabelledPetriNetSimpleWeights, StochasticLabelledPetriNetEditable {
 
 	public void setTransitionWeight(int transition, double weight);
 
