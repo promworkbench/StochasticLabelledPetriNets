31b498cc8c3f28df19c706ad61e976e54b8c158f
Sander Leemans <bla@bla.com>
2023-12-21 11:15:32 +0100

    remove basic miner as it introduced circular dependencies

--- src/org/processmining/stochasticlabelledpetrinets/basicminer/BasicStochasticMinerParameters.java
+++
@@ -1,7 +0,0 @@
-package org.processmining.stochasticlabelledpetrinets.basicminer;
-
-import org.deckfour.xes.classification.XEventClassifier;
-
-public interface BasicStochasticMinerParameters {
-	public XEventClassifier getClassifier();
-}
